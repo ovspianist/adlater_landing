16b630d66174fafa8ba31a04e2425efc4d5416d3
Chuang Niu <ovspianist@gmail.com>
2023-04-23 16:41:57 +0100

    select none on pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="flex flex-col w-full items-center py-3 lg:py-12 bg-gradient-to-b from-neutral-900 to-transparent">
+    <div className="flex flex-col w-full items-center py-3 lg:py-12 bg-gradient-to-b from-neutral-900 to-transparent select-none">
       <div className="flex flex-row w-full lg:w-[1200px] max-w-[90vw] justify-between items-center px-1 py-2 border-0 border-red-500 ">
         <div>
           <Link href="/" className="flex flex-row gap-3 items-center text-xl font-bold cursor-pointer">
--- pages/dshrt.tsx
+++ pages/dshrt.tsx
@@ -15,7 +15,7 @@ function showreel() {
             <div className="min-h-[100vh]">
               <Navbar />
 
-              <div className="flex flex-col w-full h-[450px] md:h-[550px] lg:h-[600px] px-8 items-center">
+              <div className="flex flex-col w-full h-[450px] md:h-[550px] lg:h-[600px] px-8 items-center select-none">
                 <h1 className="text-2xl lg:text-[32px] pt-8 md:pt-4 lg:pt-0 pb-8">Showreel ﹒ Investor</h1>
                 <iframe
                     src="https://player.vimeo.com/video/689902544?h=1c8590a277"
@@ -30,7 +30,7 @@ function showreel() {
                 <p className="text-md font-light lg:text-lg py-5 text-neutral-400 ">Password is provided with pitch deck or available upon request.</p>
               </div>
               <Divider />
-              <div className="flex flex-col items-center gap-2 pt-0 pb-10">
+              <div className="flex flex-col items-center gap-2 pt-0 pb-10 select-none">
                 <a href="https://forms.gle/bbr2mkbvTcRHP5WD7" target="_blank">
                 <button className="text-xl py-4 px-8 border-t-[1px] border-t-neutral-400 bg-gradient-to-r from-red-500 to-indigo-700 rounded-xl shadow-xl shadow-[rgba(0,0,0,0.2)]">Join our mailing list</button>
                 </a>
