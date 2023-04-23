bb5b83952d8927e93a190b61a82a15a31c1bf8cf
Chuang Niu <ovspianist@gmail.com>
2023-04-23 16:27:22 +0100

    put google font links inside head tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;700&display=swap" rel="stylesheet" />
       </Head>
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;700&display=swap" rel="stylesheet" />
+
       <body>
         <Main />
         <NextScript />
