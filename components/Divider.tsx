5e390504a04a20d8563239973d8685ec16deffe4
Chuang Niu <ovspianist@gmail.com>
2023-04-23 16:16:46 +0100

    copied the new site to dev branch

---
+++ components/Divider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Divider() {
+  return (
+    <div className="flex flex-col w-full items-center">
+      <div className="w-[400px] lg:w-[1000px] border-t-2 border-[rgba(200,200,200,0.2)] my-8"></div>
+    </div>
+  )
+}
+
+export default Divider
